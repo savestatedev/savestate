e70b7e8550c8152f1ad97742299234693893436f
DB Hurley <dbhurley@me.com>
2026-08-25 01:01:46 -0400

    Print excluded paths when the import passphrase is wrong

--- src/commands/container.ts
+++ src/commands/container.ts
@@ -1033,6 +1033,9 @@ export async function importState(options: RestoreOptions): Promise<ImportResult
       if (listedComponents) {
         console.error(formatImportComponents(listedComponents));
       }
+      if (excludedComponents) {
+        console.error(formatImportExcluded(excludedComponents));
+      }
       return undefined;
     }
     
